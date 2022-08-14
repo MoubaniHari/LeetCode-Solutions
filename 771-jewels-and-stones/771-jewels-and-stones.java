dc38d118d565920e560a53632074ad51df7432f9
Moubani Hari <86869324+MoubaniHari@users.noreply.github.com>
2022-08-14 12:12:26 +0530

    Time: 4 ms (15.08%), Space: 42.3 MB (53.79%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        Map<Character,Integer> map=new HashMap<>();
+        for(char c: jewels.toCharArray())
+            map.put(c,map.getOrDefault(c,0)+1);
+        for(char c: stones.toCharArray())
+            if(map.containsKey(c)) count++;
+        return count;
+    }
+}
