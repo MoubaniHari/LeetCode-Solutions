b640b718a19500b3791eec40e1c8d2d5103dac3d
Moubani Hari <86869324+MoubaniHari@users.noreply.github.com>
2022-08-15 13:42:55 +0530

    Time: 5 ms (83.74%), Space: 42.2 MB (95.20%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,27 +1,14 @@
 class Solution {
     public String decodeMessage(String key, String message) {
-        // Map<Character,Character> map=new HashMap<>();
-        // char ch='a';
-        // map.put(' ',' ');
-        // for(char c:key.toCharArray())
-        //     if(!map.containsKey(c)) 
-        //         map.put(c,ch++);
-        // StringBuilder sb = new StringBuilder();
-        // for(char c:message.toCharArray())
-        //     sb.append(map.get(c));
-        // return sb.toString();
-        HashMap < Character, Character > map = new HashMap < > ();
-        char flet = 'a';
-        for (char c: key.toCharArray()) {
-            if (!map.containsKey(c) && c != ' ') {
-                map.put(c, flet++);
-            }
-        }
-        StringBuilder result = new StringBuilder();
-        for (char c: message.toCharArray()) {
-            if (c == ' ') result.append(" ");
-            else result.append(map.get(c));
-        }
-        return result.toString();
+        Map<Character,Character> map=new HashMap<>();
+        char ch='a';
+        map.put(' ',' ');
+        for(char c:key.toCharArray())
+            if(!map.containsKey(c)) 
+                map.put(c,ch++);
+        StringBuilder sb = new StringBuilder();
+        for(char c:message.toCharArray())
+            sb.append(map.get(c));
+        return sb.toString();
     }
 }
