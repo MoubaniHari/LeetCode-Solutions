0398e49bba30388dec3e54913068ccad35bc3d10
Moubani Hari <86869324+MoubaniHari@users.noreply.github.com>
2022-08-15 13:38:47 +0530

    Time: 8 ms (65.31%), Space: 42.6 MB (86.39%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,18 +1,14 @@
 class Solution {
     public String decodeMessage(String key, String message) {
         Map<Character,Character> map=new HashMap<>();
-        int i=0;
-        for(char c:key.toCharArray()){
-            if(c==' ') continue;
-            else if(!map.containsKey(c)){ 
-                map.put(c,(char)(i+97));
-                i++;
-            }
-        }
-        System.out.println(map);
+        char ch='a';
+        map.put(' ',' ');
+        for(char c:key.toCharArray())
+            if(!map.containsKey(c)) 
+                map.put(c,ch++);
         String dec="";
         for(char c:message.toCharArray())
-            if(c==' ') dec+=' '; else dec+=map.get(c);
+            dec+=map.get(c);
         return dec;
     }
 }
