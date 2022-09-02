654dd0b1ebcd9ce04543ef8415039c892f26e2b2
Moubani Hari <86869324+MoubaniHari@users.noreply.github.com>
2022-09-02 21:32:09 +0530

    Time: 1 ms (82.37%), Space: 41.6 MB (84.96%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,22 +1,13 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        int j;
-        int[] target = new int[nums.length];
-        for(int i=0;i<target.length;i++)
-            target[i]=-1;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(target[index[i]]==-1)
-                target[index[i]]=nums[i];
-            else{
-                j=target.length-1;
-                while(j>index[i]){
-                    target[j]=target[j-1];
-                    j--;
-                }
-                target[index[i]]=nums[i];
+        for(int i=0;i<nums.length;i++){
+            int j=i,temp=nums[i];
+            while(j>index[i]){
+                nums[j]=nums[j-1];
+                j--;
             }
+            nums[index[i]]=temp;
         }
-        return target;
+        return nums;
     }
 }
