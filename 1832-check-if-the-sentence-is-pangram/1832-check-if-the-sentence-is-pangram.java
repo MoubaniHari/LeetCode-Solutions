84e8143764623bb3cd1491150c60812e61f4f278
Moubani Hari <86869324+MoubaniHari@users.noreply.github.com>
2022-09-25 20:59:13 +0530

    Time: 23 ms (6.39%), Space: 41.8 MB (77.07%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int i;
+        Map<Character,Integer> map=new HashMap<>();
+        for(i=0;i<sentence.length();i++)
+            map.put(sentence.charAt(i),map.getOrDefault(sentence.charAt(i),0)+1);
+        for(i=97;i<=122;i++){
+            if(!map.containsKey((char)i))
+                return false;
+        }
+        return true;
+    }
+}
